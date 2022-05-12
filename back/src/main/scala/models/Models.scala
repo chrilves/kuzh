06289ad325534c70fbd6b6b7a749b0efcc3bd8c1
chrilves <christophe.calves@gmail.com>
2022-05-12 15:29:53 +0200

    WIP

---
+++ back/src/main/scala/models/Models.scala
@@ -0,0 +1,99 @@
+package models
+
+package chrilves.kuzhback
+
+import cats.Applicative
+import cats.implicits.*
+import io.circe.{Encoder, Json}   
+
+object Assembly:
+    enum Harvest[A]:
+        val path: List[Member.Fingerprint]
+
+        case Question(path: Member.Fingerprint) extends Harvest[String]
+        case Answer(question: String, path: List[Member.Fingerprint]) extends Harvest[Boolean]
+
+    object Harvest:
+        opaque type Signed = String
+
+        final case class ValidationOk(estimation: Long, signature: Harvest.Signed)
+
+        final case class Result[A](
+            harvest: Harvest[A],
+            hashes: Set[Secret.Hash],
+            validations: Map[Member.Fingerprint, ValidationOk],
+            value: Set[Secret[A]]
+        )           
+    
+    enum Secret[A]:
+        val random: String
+
+        case Question(value: String, random: String) extends Secret[String]
+        case Answer(value: Boolean, random: String) extends Secret[Boolean]
+
+    object Secret:
+        opaque type Hash = String
+    
+    object Harvesting:
+        enum FailureReason:
+            case Refused(members: Set[Member.Fingerprint])
+            case Invalid(members: Set[Member.Fingerprint])
+            case MembersMissing(member: Set[Member.Fingerprint])
+            case ProtocolError(member: Member.Fingerprint)
+
+        enum Phase[A]:
+            case Accept(
+                harvest: Harvest[A],
+                consents: Map[Member.Fingerprint, Harvest.Signed]
+            )
+            case Hash(
+                harvest: Harvest[A],
+                consents: Map[Member.Fingerprint, Harvest.Signed],
+                nexts: List[Member.Fingerprint]
+            )
+            case Validation(
+                harvest: Harvest[A],
+                hashes: Set[Secret.Hash],
+                validations: Map[Member.Fingerprint, Harvest.ValidationOk]
+            )
+            case Real(
+                harvest: Harvest[A],
+                hashes: Set[Secret.Hash],
+                validations: Map[Member.Fingerprint, Harvest.ValidationOk],
+                nexts: List[Member.Fingerprint]
+            )
+            case Failed(reason: FailureReason)
+            case Closing[A](
+                result: Harvest.Result[A],
+                waiting: Set[Member.Fingerprint]
+            ) extends Phase[A]
+
+    enum WaitingStatus:
+        case Budy, Ready
+
+    enum Status:
+        case Waiting(members: Map[Member.Fingerprint, WaitingStatus])
+        case Harvesting(phase: Assembly.Harvesting.Phase[_])
+    
+    final case class State(
+        questions: List[String],
+        members: Map[Member.Fingerprint, Member.Presence],
+        scores: Map[Member.Fingerprint, Long],
+        status: Status
+    ):
+        def question: Option[String] = questions.headOption
+
+object Member:
+    opaque type PK = String
+    opaque type Fingerprint = String
+
+    extension (publicKey: PK) def fingerprint: Fingerprint = ???
+    
+    enum Presence:
+        case Absent, Present
+
+object MemberChannel:
+    enum Connection:
+        case Initial
+        case ChallengeSent(publicKey: Member.PK, challenge: String)
+        case Verified(publicKey: Member.PK)
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,25 +2,57 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type AppState = {
+  last_assembly: Boolean
 }
 
-export default App;
+export default class App extends React.Component<{}, AppState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = { last_assembly: true }
+  }
+
+  render(): JSX.Element {
+    return <div className="App">
+            <header className="App-header">
+              <h1>Kuzh</h1>
+              { this.state.last_assembly ?
+                <div className="kuzh-rejoin-last-assembly">
+                  <h2 className="kuzh-style-action">Revenir à la dernière Assemblée</h2>
+                  <form>
+                    <input type="button" name="join_last_assembly" value="Revenir à la dernière Assemblée"/>
+                  </form>
+                </div>
+                : null
+              }
+              <div className="kuzh-join-assembly">
+                <h2 className="kuzh-style-action">Rejoindre une Assemblée existante</h2>
+                <form>
+                  <input type="text" name="assembly_key" placeholder="clef de connexion de l'assemblée"></input>
+                  <input type="button" name="join_assembly" value="Rejoindre l'Assemblée"/>
+                </form>
+              </div>
+              <div className="kuzh-create-assembly">
+                <h2 className="kuzh-style-action">Créer une nouvelle Assemblée</h2>
+                <form>
+                  <input type="text" name="assembly_name" placeholder="nom de l'assemblée"></input>
+                  <input type="button" name="create_assembly" value="Créer l'Assemblée"/>
+                </form>
+              </div>
+
+              {/* <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.tsx</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+            </a> */}
+            </header>
+          </div>;
+  }
+}
