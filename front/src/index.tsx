9a5054578a26bc086d8a6c7564e9cb512ed3400e
chrilves <christophe.calves@gmail.com>
2022-08-27 01:34:20 +0200

    PWA

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -17,7 +17,8 @@ import {
 } from "./services/IdentityProofStore";
 import { Services } from "./services/Services";
 import { LocalStorageAPI, StorageAPI } from "./services/StorageAPI";
-//import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 import "./style.css";
 import "./i18n";
 
@@ -57,7 +58,12 @@ root.render(
   </React.StrictMode>
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
+reportWebVitals();
